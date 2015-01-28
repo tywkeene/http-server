3b863e0f2d4fa0a055d11cce15bf0fe136ed729e
SaviorPhoenix <tyrell.wkeene@gmail.com>
2015-01-28 06:27:37 -0700

    Added http.ListenAndServeTLS() to server.go, use flags to parse necessary command line options. Added key.pem and cert.pem to .gitignore

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/SaviorPhoenix/http-server/cache"
 	"github.com/SaviorPhoenix/http-server/data"
 	"github.com/SaviorPhoenix/http-server/handles"
@@ -8,7 +9,31 @@ import (
 	"net/http"
 )
 
+type Options struct {
+	port     *string
+	useTls   *bool
+	certPath *string
+	certKey  *string
+}
+
+var Opts *Options
+
 func init() {
+	Opts = &Options{
+		flag.String("port", "", "Bind port (Default 80 for HTTP, 443 for HTTPS)"),
+		flag.Bool("use-tls", false, "Use SSL/TLS (https)"),
+		flag.String("cert", "./cert.pem", "Path to a signed SSL/TLS certificate"),
+		flag.String("cert-key", "./key.pem", "Path to the key associated with the signed SSL/TLS certificate"),
+	}
+	flag.Parse()
+
+	//Set the default port depending on if we're serving HTTP or HTTPS
+	if *Opts.useTls == true && *Opts.port == "" {
+		*Opts.port = "443"
+	} else if *Opts.useTls == false && *Opts.port == "" {
+		*Opts.port = "80"
+	}
+
 	//Initialize the data-getter map for the documents
 	data.InitGetters()
 
@@ -24,8 +49,15 @@ func init() {
 }
 
 func main() {
-	log.Println("Listening on port 3000")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
-		panic(err)
+	if *Opts.useTls == true {
+		log.Println("Accepting HTTPS on port:", *Opts.port)
+		if err := http.ListenAndServeTLS(":"+*Opts.port, *Opts.certPath, *Opts.certKey, nil); err != nil {
+			panic(err)
+		}
+	} else {
+		log.Println("Accepting HTTP on port:", *Opts.port)
+		if err := http.ListenAndServe(":"+*Opts.port, nil); err != nil {
+			panic(err)
+		}
 	}
 }
